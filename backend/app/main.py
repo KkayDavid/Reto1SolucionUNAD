c5f80997e621f29da583352d1f852033ce10a9be
KkayDavid <87913955+KkayDavid@users.noreply.github.com>
2025-12-05 22:52:48 -0500

    Add new origin to CORS middleware configuration

--- backend/app/main.py
+++ backend/app/main.py
@@ -38,10 +38,11 @@ app = FastAPI(title="Correo Gestión - Backend")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",
-        "https://reto1-solucion-unad.vercel.app"
-    ],
+   allow_origins=[
+    "http://localhost:3000",
+    "https://reto1-solucion-unad.vercel.app",
+    "https://reto1-solucion-unad-7xtrc585d-kkaydavids-projects.vercel.app"
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
